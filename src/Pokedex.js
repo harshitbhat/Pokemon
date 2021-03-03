d8c2914bd1d1f28c496995b6ef2e2eb23c8d2792
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-03-03 17:45:06 +0530

    Populating the cards in two decks

---
+++ src/Pokedex.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Pokecard from './Pokecard';
+import './Pokedex.css';
+
+class Pokedex extends Component {
+  render() {
+    const isWinner = this.props.isWinner;
+    let title = ``;
+    if (isWinner) {
+      title = <h1 className="Pokedex-winner">Winning Hand</h1>;
+    } else {
+      title = <h1 className="Pokedex-loser">Losing Hand</h1>;
+    }
+    return (
+      <div className="Pokedex">
+        {title}
+        <h4>Total Experience: {this.props.exp}</h4>
+        <div className="Pokedex-cards">
+          {this.props.pokemon.map((p) => (
+            <Pokecard
+              id={p.id}
+              name={p.name}
+              type={p.type}
+              experience={p.base_experience}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Pokedex;
